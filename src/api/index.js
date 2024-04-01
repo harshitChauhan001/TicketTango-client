4b51331a7c012c3693d50c1d951c0313d871d285
harshitChauhan001 <108900696+harshitChauhan001@users.noreply.github.com>
2024-04-01 09:35:27 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3000" });
+const API = axios.create({ baseURL: "https://electric-blue-worm-hose.cyclic.app/" });
 
 export const fetchAllMovies = () => API.get("/movie/allMovies");
 export const fetchMovieById = (id) => API.get(`/movie/${id}`);
